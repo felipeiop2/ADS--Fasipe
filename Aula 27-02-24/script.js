68f9d12b74d578d8a43e580f82dd6c422abd028c
felipeiop2 <154385972+felipeiop2@users.noreply.github.com>
2024-02-28 10:35:42 -0300

    Aula 27-02-24

---
+++ Aula 27-02-24/script.js
@@ -0,0 +1,8 @@
+let botao = window.document.getElementById('botao')
+let res = window.document.getElementById('res')
+function botao(){
+    if(botao = true) {
+        res.innerHTML = ('KKKKK')        
+    
+    }
+}
