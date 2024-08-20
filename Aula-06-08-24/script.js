910745e4df8a2c74b2bca9ba92a570c6c8eb1ec6
felipeiop2 <154385972+felipeiop2@users.noreply.github.com>
2024-08-20 20:26:55 -0300

    barreto

---
+++ Aula-06-08-24/script.js
@@ -0,0 +1,8 @@
+var num1 = window.prompt('Digite um numero')
+var num2 = window.prompt('Digite outro numero')
+if(num1 > num2){
+    alert("O primeiro numero é maior que o segundo")
+}else{
+    alert("O primeiro numero nao é maior que o segundo")
+}
+alert("fim")
